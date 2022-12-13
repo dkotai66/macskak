1155cdd49f02053b08cee8b461d5eb7e55a5f1c0
dani <dkotai66@gmail.com>
2022-12-13 13:26:24 +0100

    feladat befejezve

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
 import {
+  Body,
   Controller,
   Get,
+  Param,
+  Post,
+  Redirect,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
+import { Macskak } from './macskak.dto';
 
 @Controller()
 export class AppController {
@@ -12,7 +17,28 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async Macskak() {
+    const [rows] = await db.execute('SELECT * FROM macskak ORDER BY suly ASC');
+    return {
+      macskak: rows,
+    };
+  }
+
+  @Get('macskak/new')
+  @Render('felvetel')
+  newPaintingForm() {
+    return {};
+  }
+
+  @Post('macskak/new')
+  @Redirect()
+  async newPainting(@Body() macskak: Macskak) {
+    const [result]: any = await db.execute(
+      'INSERT INTO macskak (szem_szin, suly) VALUES (?, ?)',
+      [macskak.szem_szin, macskak.suly],
+    );
+    return {
+      url: '/',
+    };
   }
 }
---
+++ src/macskak.dto.ts
@@ -0,0 +1,4 @@
+export class Macskak {
+  suly: number;
+  szem_szin: string;
+}
